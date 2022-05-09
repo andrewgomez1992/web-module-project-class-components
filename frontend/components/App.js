61a3f33858df4b604490f97edf626d7289ccdc88
Andrew Gomez <drewgomez209@gmail.com>
2022-05-09 11:23:34 -0700

    added all dom

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,22 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <ul>
+          <li>Workout</li>
+          <li>Bake Myself</li>
+          <li>Deshed Dogs</li>
+        </ul>
+        <form>
+          <input
+            type='text'
+            name='todo'
+          // value={}
+          // onChange={}  
+          />
+          <button>Add Todo</button>
+
+        </form>
+        <button>Clear Completed</button>
       </div>
     )
   }
--- frontend/index.js
+++ frontend/index.js
@@ -1,13 +1,36 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './components/App'
+import Form from './components/Form'
 import './styles/reset.css'
 import './styles/styles.css'
+
+const todos = [
+  {
+    name: 'Organize Garage',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    name: 'Bake Cookies',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    name: 'Deshed Dogs',
+    id: Date.now(),
+    completed: false
+  }
+]
+
+
+
 
 render(
   <React.StrictMode>
     <h1>Todo App</h1>
     <App />
+    <Form />
   </React.StrictMode>
   , document.getElementById('root')
 )
